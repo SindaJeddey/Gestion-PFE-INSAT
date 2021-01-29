02d5403a147d994b00c040d81b9f128a9ef4f33f
sinda <sindajeddey@gmail.com>
2021-01-29 10:21:22 +0100

    Setting Database connection and creating authentication module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.DATABASE_URI),
+    AuthenticationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
